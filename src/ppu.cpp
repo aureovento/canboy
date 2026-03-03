9d91fede1e38b53766b3ac083831368aa89e3daf
onecancat <onecancat@gmail.com>
2026-03-03 12:14:07 +0530

    fixed mode 3 length to be based on dotcount or pixels

--- src/ppu.cpp
+++ src/ppu.cpp
@@ -61,7 +61,7 @@ void PPU::tick() {
 	uint8_t prevMode = mode;
 	if (ly <= 143) {
 		if (dotcount < 80) mode = 2;
-		else if (dotcount < 252) mode = 3;
+		else if (dotcount < 252 || xPixel < 160) mode = 3;
 		else mode = 0;
 	} else {
 		mode = 1;
