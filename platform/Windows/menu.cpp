50fa144dcadfdfc314ebe09ed4a77b59918dba8d
onecancat <onecancat@gmail.com>
2026-03-16 22:57:36 +0530

    recent roms list

--- platform/Windows/menu.cpp
+++ platform/Windows/menu.cpp
@@ -2,6 +2,8 @@
 #include "Emu.h"
 #include <nfd.h>
 #include <iostream>
+#include <fstream>
+#include <algorithm>
 
 static Emu* emu = nullptr;
 
@@ -9,13 +11,59 @@ void setOverlay(Emu* e) {
     emu = e;
 }
 
+static HMENU RecentsMenu = nullptr;
+
+
+static std::vector<std::string> loadRecents() {
+    std::vector<std::string> list;
+    std::ifstream f("recents.txt");
+    std::string line;
+    while (std::getline(f, line) && (int)list.size() < RECENT_COUNT) {
+        if (!line.empty()) list.push_back(line);
+    }
+    return list;
+}
+
+static void saveRecents(const std::vector<std::string>& list) {
+    std::ofstream f("recents.txt");
+    for (auto& s : list) f << s << '\n';
+}
+
+void addRecentROM(const std::string& path) {
+    auto list = loadRecents();
+    list.erase(std::remove(list.begin(), list.end(), path), list.end());
+    list.insert(list.begin(), path);
+    if ((int)list.size() > RECENT_COUNT) list.resize(RECENT_COUNT);
+    saveRecents(list);
+}
+
+static void rebuildRecentsMenu() {
+    while (GetMenuItemCount(RecentsMenu) > 0)
+        RemoveMenu(RecentsMenu, 0, MF_BYPOSITION);
+
+    auto list = loadRecents();
+    if (list.empty()) {
+        AppendMenuA(RecentsMenu, MF_STRING | MF_GRAYED, 0, "(no recent ROMs)");
+        return;
+    }
+    for (int i = 0; i < (int)list.size(); i++) {
+        std::string label = list[i];
+        size_t sep = label.find_last_of("\\/"); // to get only the file name
+        if (sep != std::string::npos) label = label.substr(sep + 1);
+        AppendMenuA(RecentsMenu, MF_STRING, RECENT_BASE + i, label.c_str());
+    }
+}
+
+
 void createMenu(HWND hwnd)
 {
     HMENU menubar = CreateMenu();
     HMENU fileMenu = CreateMenu();
     HMENU emuMenu = CreateMenu();
+    RecentsMenu = CreatePopupMenu();
 
     AppendMenu(fileMenu, MF_STRING, FILE_OPEN, "Load ROM");
+    AppendMenu(fileMenu, MF_POPUP, (UINT_PTR)RecentsMenu, "Recents");
     AppendMenu(emuMenu, MF_STRING, EMU_PAUSE, "Pause\tSpace");
     AppendMenu(emuMenu, MF_SEPARATOR, 0, NULL);
     AppendMenu(emuMenu, MF_STRING, EMU_EXIT, "Exit");
@@ -30,12 +78,27 @@ void createMenu(HWND hwnd)
 
 LRESULT CALLBACK MenuWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
+    case WM_INITMENUPOPUP: {                  
+        if ((HMENU)wParam == RecentsMenu) rebuildRecentsMenu();             
+        break;
+    }
     case WM_COMMAND: {
+        int id = LOWORD(wParam);
+        if (id >= RECENT_BASE && id < RECENT_BASE + RECENT_COUNT) {
+            auto list = loadRecents();
+            int idx = id - RECENT_BASE;
+            if (idx < (int)list.size()) {
+                std::string path = list[idx];
+                if (emu->loadCart(path)) addRecentROM(path);         
+            }
+            break;
+        }
         switch (LOWORD(wParam)) {
         case FILE_OPEN: {
             nfdchar_t* outPath = nullptr;
             if (NFD_OpenDialog(NULL, NULL, &outPath) == NFD_OKAY) {
-                emu->loadCart(outPath);
+                std::string path(outPath);
+                if (emu->loadCart(path)) addRecentROM(path);
                 free(outPath);
             }
             break;
--- platform/Windows/menu.h
+++ platform/Windows/menu.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Windows.h>
+#include <string>
 
 class Emu;
 void setOverlay(Emu* emu);
@@ -7,7 +8,10 @@ void setOverlay(Emu* emu);
 #define FILE_OPEN   1001
 #define EMU_EXIT   1002
 #define EMU_PAUSE   1003
+#define RECENT_BASE    2001
+#define RECENT_COUNT 5
 
 static WNDPROC ogWndProc = nullptr;
 void createMenu(HWND hwnd);
+void addRecentROM(const std::string& path);
 LRESULT CALLBACK MenuWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
